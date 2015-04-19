e2d2eb95921bbd28dc33b3e164026d34cd00e1ae
earlwlkr <mike358015@gmail.com>
2015-04-19 17:54:56 +0700

    Remove action and view of Contact

--- GalaxyComputersASP/Controllers/HomeController.cs
+++ GalaxyComputersASP/Controllers/HomeController.cs
@@ -15,15 +15,6 @@ namespace GalaxyComputersASP.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
