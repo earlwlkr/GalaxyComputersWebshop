1bc8b99162fc6790d2738c04d9ed273af966c729
earlwlkr <mike358015@gmail.com>
2015-05-05 17:47:46 +0700

    Add currency format, update culture

--- GalaxyComputersASP/Models/Product.cs
+++ GalaxyComputersASP/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,7 @@ namespace GalaxyComputersASP.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public double Price { get; set; }
         public string Description { get; set; }
         public DateTime PublishDate { get; set; }
