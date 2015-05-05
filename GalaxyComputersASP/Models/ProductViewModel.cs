25cfee9015d6e75676b0e697b5f619b200991d92
earlwlkr <mike358015@gmail.com>
2015-05-05 11:54:27 +0700

    Create ProductDetailsViewModel

---
+++ GalaxyComputersASP/Models/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GalaxyComputersASP.Models
+{
+    public class ProductDetailsViewModel
+    {
+        public Category Category { get; set; }
+        public Product Product { get; set; }
+    }
+}
