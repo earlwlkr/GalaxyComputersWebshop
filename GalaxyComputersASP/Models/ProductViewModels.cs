3580efc23aaf7d658b40172d023298fbf60270d6
earlwlkr <mike358015@gmail.com>
2015-05-05 21:01:23 +0700

    Add ProjectOverview viewmodel

--- GalaxyComputersASP/Models/ProductViewModels.cs
+++ GalaxyComputersASP/Models/ProductViewModels.cs
@@ -5,6 +5,12 @@ using System.Web;
 
 namespace GalaxyComputersASP.Models
 {
+    public class ProductOverview
+    {
+        public Category Category { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public Product Product { get; set; }
+    }
     public class ProductDetailsViewModel
     {
         public Category Category { get; set; }
