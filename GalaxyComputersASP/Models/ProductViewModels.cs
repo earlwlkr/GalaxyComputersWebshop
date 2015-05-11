8126dd41f6d033086d2c3e9dd3877f3d53734c09
earlwlkr <mike358015@gmail.com>
2015-05-11 21:36:36 +0700

    Create ViewModel for PartialIndex

--- GalaxyComputersASP/Models/ProductViewModels.cs
+++ GalaxyComputersASP/Models/ProductViewModels.cs
@@ -11,6 +11,14 @@ namespace GalaxyComputersASP.Models
         public Manufacturer ProductManufacturer { get; set; }
         public Product ProductData { get; set; }
     }
+    public class PartialIndexViewModel
+    {
+        public IEnumerable<Product> Products;
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int ItemsPerPage { get; set; }
+        public Nullable<int> CategoryID { get; set; }
+    }
     public class ProductManageViewModel
     {
         public IEnumerable<ProductOverview> Products;
