dfca4250a6e6cfe6f613a466ab47291ebaafb251
earlwlkr <mike358015@gmail.com>
2015-05-11 12:43:01 +0700

    Load categories menu from database

--- GalaxyComputersASP/Controllers/CategoriesController.cs
+++ GalaxyComputersASP/Controllers/CategoriesController.cs
@@ -41,6 +41,12 @@ namespace GalaxyComputersASP.Controllers
             return View();
         }
 
+        // GET: Categories/CategoriesMenu
+        public ActionResult CategoriesMenu()
+        {
+            return PartialView(db.Categories.ToList());
+        }
+
         // POST: Categories/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
