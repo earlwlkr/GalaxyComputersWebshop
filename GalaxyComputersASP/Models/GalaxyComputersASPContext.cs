6dfada78b58561006e448c446ac22bf6ecb6397f
earlwlkr <mike358015@gmail.com>
2015-05-22 20:33:42 +0700

    Inherit main DbContext from IdentityDbContext

--- GalaxyComputersASP/Models/GalaxyComputersASPContext.cs
+++ GalaxyComputersASP/Models/GalaxyComputersASPContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Web;
 
 namespace GalaxyComputersASP.Models
 {
-    public class GalaxyComputersASPContext : DbContext
+    public class GalaxyComputersASPContext : IdentityDbContext
     {
         // You can add custom code to this file. Changes will not be overwritten.
         // 
