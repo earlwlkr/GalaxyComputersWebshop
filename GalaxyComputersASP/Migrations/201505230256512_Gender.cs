544fba565121164c9605f7fde896cf7ec6aac370
earlwlkr <mike358015@gmail.com>
2015-05-23 10:05:35 +0700

    Add Gender to user model

---
+++ GalaxyComputersASP/Migrations/201505230256512_Gender.cs
@@ -0,0 +1,18 @@
+namespace GalaxyComputersASP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Gender : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Gender", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Gender");
+        }
+    }
+}
--- GalaxyComputersASP/Models/IdentityModels.cs
+++ GalaxyComputersASP/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace GalaxyComputersASP.Models
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Gender { get; set; }
         public string Address { get; set; }
         public DateTime Birthdate { get; set; }
     }
