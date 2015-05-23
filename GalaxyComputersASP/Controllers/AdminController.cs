ab35abd56844c2f79bbc6d0549cef05cfe226010
earlwlkr <mike358015@gmail.com>
2015-05-23 15:39:21 +0700

    Add AdminController

---
+++ GalaxyComputersASP/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using GalaxyComputersASP.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GalaxyComputersASP.Controllers
+{
+    public class AdminController : Controller
+    {
+        private GalaxyComputersASPContext db = new GalaxyComputersASPContext();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(ApplicationDbContext.Create()));
+            IEnumerable<ApplicationUser> users = userManager.Users;
+
+            IEnumerable<Product> products = db.Products.ToList();
+            List<ProductOverview> list = new List<ProductOverview>();
+
+            foreach (Product product in products)
+            {
+                Category category = db.Categories.Find(product.CategoryID);
+                Manufacturer manufacturer = db.Manufacturers.Find(product.ManufacturerID);
+                list.Add(new ProductOverview { ProductData = product, ProductCategory = category, ProductManufacturer = manufacturer });
+            }
+            return View(new AdminViewModel
+            {
+                Users = users.ToList(),
+                Products = list.ToList(),
+                Categories = db.Categories.ToList(),
+                Manufacturers = db.Manufacturers.ToList()
+            });
+        }
+    }
+}
---
+++ GalaxyComputersASP/Models/AdminViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GalaxyComputersASP.Models
+{
+    public class AdminViewModel
+    {
+        public IEnumerable<ApplicationUser> Users;
+        public IEnumerable<ProductOverview> Products;
+        public IEnumerable<Category> Categories;
+        public IEnumerable<Manufacturer> Manufacturers;
+    }
+}
