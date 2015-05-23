c364e08f7117a5ab96aaeabcc888f938e7e65006
earlwlkr <mike358015@gmail.com>
2015-05-23 16:35:57 +0700

    Add Roles dropdownlist to set for users

--- GalaxyComputersASP/Models/AdminViewModels.cs
+++ GalaxyComputersASP/Models/AdminViewModels.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace GalaxyComputersASP.Models
 {
     public class AdminViewModel
     {
         public IEnumerable<ApplicationUser> Users;
+        public SelectList Roles;
         public IEnumerable<ProductOverview> Products;
         public IEnumerable<Category> Categories;
         public IEnumerable<Manufacturer> Manufacturers;
