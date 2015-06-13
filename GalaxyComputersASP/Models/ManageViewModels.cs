ef59c44e60abb738513a51e74efaab1f9113da53
earlwlkr <mike358015@gmail.com>
2015-06-13 17:31:27 +0700

    Change password validation messages

--- GalaxyComputersASP/Models/ManageViewModels.cs
+++ GalaxyComputersASP/Models/ManageViewModels.cs
@@ -41,20 +41,20 @@ namespace GalaxyComputersASP.Models
 
     public class ChangePasswordViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Bạn chưa nhập Mật khẩu hiện tại!")]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Mật khẩu hiện tại")]
         public string OldPassword { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = "Bạn chưa nhập Mật khẩu mới!")]
+        [StringLength(100, ErrorMessage = "{0} phải dài ít nhất {2} ký tự.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Mật khẩu mới")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Nhập lại mật khẩu mới")]
+        [Compare("NewPassword", ErrorMessage = "Mật khẩu mới không trùng nhau.")]
         public string ConfirmPassword { get; set; }
     }
 
